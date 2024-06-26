aee17393a4e8ff104ee3514f448d22fd2d8a5ffe
Clivaly Cardoso <clivalydev@gmail.com>
2024-06-26 07:48:35 -0300

    Implement query_params

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,6 +4,7 @@ mod hello_world;
 mod mirror_body_string;
 mod mirror_body_json;
 mod path_variables;
+mod query_params;
 
 use axum::{
     body::Body,
@@ -15,6 +16,7 @@ use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
 
 // main function to create our routes
 pub fn create_routes() -> Router<Body> {
@@ -24,4 +26,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/:id", get(path_variables))
         .route("/path_variables/11", get(hard_coded_path))
+        .route("/query_params/", get(query_params))
 }
